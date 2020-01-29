52339beae617d19ae1ed69f272313a07b3048597
yq <2867661200@qq.com>
2020-01-29 18:37:24 +0800

    yq3

---
+++ movie-front-boot/src/main/java/com/fh/MovieFrontBootApplication.java
@@ -0,0 +1,13 @@
+package com.fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFrontBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFrontBootApplication.class, args);
+    }
+
+}
